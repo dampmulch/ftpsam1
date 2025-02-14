8dc26a9f22643c887791fa3bb69cc759ab2bc182
Shehab <114194812+dampmulch@users.noreply.github.com>
2025-02-14 13:45:30 -0500

    nothing

--- next.config.js
+++ next.config.js
@@ -1,1 +1,22 @@
- 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
+  trailingSlash: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    webpackBuildWorker: true,
+    parallelServerBuildTraces: true,
+    parallelServerCompiles: true,
+  }
+}
+
+module.exports = nextConfig 
